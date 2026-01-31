596126ec87b8b389c68b0b31bff0791cce039811
Benjamin Larkey <spartinofarrows@gmail.com>
2026-01-31 17:19:34 -0600

    updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: 'localhost', // Force localhost instead of 127.0.0.1
-		port: 5173
-	}
-});
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
+};
+
+export default config;
